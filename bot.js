69198aa34617462801c857b12617a99c1ee46dad
cstopls8 <collin.stolpa@gmail.com>
2021-07-06 00:39:03 -0500

    final touch ups

--- bot.js
+++ bot.js
@@ -3,12 +3,10 @@ const Discord = require("discord.js");
 var decode = require("decode-html");
 const axios = require("axios");
 
-
 const client = new Discord.Client();
 /* Import Discord Buttons Client */
 const discordButtons = require("discord-buttons-plugin");
 const buttonClient = new discordButtons(client);
-
 
 const { TOKEN } = process.env;
 client.on("ready", () => {
@@ -24,21 +22,32 @@ client.on("message", (message) => {
     }
   });
   if (message.content === "button") {
-    /* Generate a Cute Embed :3 */
+    /* Generate a embedded message to display info about the bot */
+    let messageImg = "https://i.imgur.com/i7KcKU8.png";
     const embed = new Discord.MessageEmbed()
-      .setTitle("Insult me")
-      .setColor("GREEN");
+      .setColor("green")
+      .setTitle("Roast Me Bot")
+      .setURL("https://github.com/cstolps8/DiscordInsultMe")
+      .setAuthor("Collin Stolpa", messageImg, "https://github.com/cstolps8/")
+      .setDescription("A bot that will direct message you roast")
+      .setThumbnail(messageImg)
+      .addFields(
+        { name: "Click the Roast Me button", value: "See what happens" },
+        { name: "\u200B", value: "\u200B" }
+      )
+      .setTimestamp()
+      .setFooter("Follow me on Github Cstolps8", messageImg);
 
     /* Generate 1st Button with "Yes" lable on it */
     const button1 = new buttonClient.MessageButton()
-      .setLabel("Yes")
+      .setLabel("Roast Me")
       .setStyle("green")
       .setID("yes");
 
     /* Generate 3rd Link Button */
     const button3 = new buttonClient.MessageButton()
       .setLabel("Join me on OnlyFans")
-      .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+      .setURL("https://bit.ly/36g9pw8");
 
     /* Send Message with button */
     buttonClient.send(null, {
@@ -54,7 +63,7 @@ let url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 
 /* Listen to buttons event with their ID */
 buttonClient.on("yes", (inta) => {
-    // axios to send get request for insult message
+  // axios to send get request for insult message
   axios
     .get(url)
     .then((response) => {
@@ -64,7 +73,6 @@ buttonClient.on("yes", (inta) => {
     })
     .then((response) => {
       // send the response
-
       let userid = inta.member.user.id;
       client.users.cache.get(userid).send(theInsult);
     })
@@ -72,8 +80,9 @@ buttonClient.on("yes", (inta) => {
       console.log(error);
     });
 });
-buttonClient.on("no", (inta) =>
-  inta.message.reply("WTF, you are the worst person, i have ever seen")
-);
+
+// buttonClient.on("no", (inta) =>
+//   inta.message.reply("WTF, you are the worst person, i have ever seen")
+// );
 
 client.login(TOKEN);
