4e6e0a27985b8bba38e54f41e62b9fc052ecf207
cstolps8 <collin.stolpa@gmail.com>
2021-08-11 22:36:26 -0500

    time for aws

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const Discord = require("discord.js");
 var decode = require("decode-html");
 const axios = require("axios");
+const insult = require("./insult")
 
 const client = new Discord.Client();
 /* Import Discord Buttons Client */
@@ -10,79 +11,81 @@ const buttonClient = new discordButtons(client);
 
 const { TOKEN } = process.env;
 client.on("ready", () => {
-  console.log("Bot is connected");
+    console.log("Bot is connected");
+
+        /* Generate a embedded message to display info about the bot */
+        let messageImg = "https://i.imgur.com/i7KcKU8.png";
+        const embed = new Discord.MessageEmbed()
+            .setColor("green")
+            .setTitle("Roast Me Bot")
+            .setURL("https://github.com/cstolps8/DiscordInsultMe")
+            .setAuthor("Collin Stolpa", messageImg, "https://github.com/cstolps8/")
+            .setDescription("A bot that will direct message you roast")
+            .setThumbnail(messageImg)
+            .addFields(
+                { name: "Click the Roast Me button", value: "See what happens" },
+                { name: "\u200B", value: "\u200B" }
+            )
+            .setTimestamp()
+            .setFooter("Follow me on Github Cstolps8", messageImg);
+
+        /* Generate 1st Button with "Yes" lable on it */
+        const button1 = new buttonClient.MessageButton()
+            .setLabel("Roast Me")
+            .setStyle("green")
+            .setID("yes");
+
+        /* Generate 3rd Link Button */
+        const button3 = new buttonClient.MessageButton()
+            .setLabel("Join me on OnlyFans")
+            .setURL("https://bit.ly/36g9pw8");
+
+        /* Send Message with button */
+        buttonClient.send(null, {
+            //   channel: message.channel.id,
+               channel: "793901584060514354",
+            embed,
+            buttons: [[button1], [button3]],
+        });
+    
 });
 
+// send someone a roast
 client.on("message", (message) => {
-  client.on("message", (message) => {
-    // If the message is "ping"
-    if (message.content === "ping") {
-      // Send "pong" to the same channel
-      message.channel.send("slut");
-    }
-  });
-  if (message.content === "button") {
-    /* Generate a embedded message to display info about the bot */
-    let messageImg = "https://i.imgur.com/i7KcKU8.png";
-    const embed = new Discord.MessageEmbed()
-      .setColor("green")
-      .setTitle("Roast Me Bot")
-      .setURL("https://github.com/cstolps8/DiscordInsultMe")
-      .setAuthor("Collin Stolpa", messageImg, "https://github.com/cstolps8/")
-      .setDescription("A bot that will direct message you roast")
-      .setThumbnail(messageImg)
-      .addFields(
-        { name: "Click the Roast Me button", value: "See what happens" },
-        { name: "\u200B", value: "\u200B" }
-      )
-      .setTimestamp()
-      .setFooter("Follow me on Github Cstolps8", messageImg);
-
-    /* Generate 1st Button with "Yes" lable on it */
-    const button1 = new buttonClient.MessageButton()
-      .setLabel("Roast Me")
-      .setStyle("green")
-      .setID("yes");
-
-    /* Generate 3rd Link Button */
-    const button3 = new buttonClient.MessageButton()
-      .setLabel("Join me on OnlyFans")
-      .setURL("https://bit.ly/36g9pw8");
-
-    /* Send Message with button */
-    buttonClient.send(null, {
-      channel: message.channel.id,
-      embed,
-      buttons: [[button1], [button3]],
-    });
-  }
+    
 });
 
 // url that will return an insult
 let url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 
+// buttonClient.on("yes", (inta) => {
+// axios to send get request for insult message
+
+//     let userid = inta.member.user.id;
+//     theInsult = insult.getData()
+//     client.users.cache.get(userid).send(theInsult);
+// });
+
 /* Listen to buttons event with their ID */
 buttonClient.on("yes", (inta) => {
-  // axios to send get request for insult message
-  axios
-    .get(url)
-    .then((response) => {
-      //get the response from url
-      theInsult = decode(response.data.insult);
-      return response.data.insult;
-    })
-    .then((response) => {
-      // send the response
-      let userid = inta.member.user.id;
-      client.users.cache.get(userid).send(theInsult);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    // axios to send get request for insult message
+    axios
+        .get(url)
+        .then((response) => {
+            //get the response from url
+            theInsult = decode(response.data.insult);
+            return response.data.insult;
+        })
+        .then((response) => {
+            // send the response
+            let userid = inta.member.user.id;
+            client.users.cache.get(userid).send(theInsult);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
-// buttonClient.on("no", (inta) =>
-//   inta.message.reply("WTF, you are the worst person, i have ever seen")
-// );
+
 
 client.login(TOKEN);
--- insult.js
+++ insult.js
@@ -2,46 +2,30 @@ const axios = require("axios");
 var decode = require('decode-html');
 const fetch = require("node-fetch")
 
-
-
-// module.exports = async () => {
-
-// }
 let url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
+async function getData() {
+    try {
+        const data = await axios.get(url)
+        return data.data.insult
+    } catch (err) {
+        console.log(err)
+    }
+}
+module.exports = {
+    getData
+}
 
+// module.exports = async (url) => {
+//     console.log("getting insult")
+//     let resp = await axios.get(url);
+//     return resp.data.insult
 
-axios.get(url)
-    .then((response) => {
-        console.log(response.data.insult)
-        return response;
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-
-// module.exports = async () => {
-//     let url = "https://evilinsult.com/generate_insult.php?lang=en&type=json"; 
-
-
-//             const response = await axios.get(url);
-//             const data = response.data;
-//             console.log("DATA: "+ data)
-//             return data
-
-
-
-//     let json = await fetch(url, {
-//         headers: {
-
-//         },
-//         method: "GET",
-//         body: JSON.stringify()
-//     })
-
-//     json = await json.json().catch(err => { })
-//     console.log("JSON: " + json)
-
-//     return json;
+//     // .then((response) => {
+//     //     console.log(response.data.insult)
+//     //     return response;
+//     // })
+//     // .catch((error) => {
+//     //     console.log(error);
+//     // });
 
 // }
